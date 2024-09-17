7ae2d1973b089b36c18d4e5ad217e607b13a18ae
irwinekabakti <irwinekabakti@gmail.com>
2024-09-17 13:27:29 +0700

    add:(hero carousel)

--- src/routes/RootLayout/RootLayout.tsx
+++ src/routes/RootLayout/RootLayout.tsx
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
+import { useEffect } from "react";
 
 const RootLayout = () => {
+  const navigate = useNavigate();
+  const user = localStorage.getItem("access_token");
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/sign-in");
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productApi } from "./query/getProducts";
 import productSlice from "./slice/products-slice";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AuthAPI } from "./query/useLogin";
 
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
+    [AuthAPI.reducerPath]: AuthAPI.reducer, // Add the login API reducer
     product: productSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productApi.middleware),
+    getDefaultMiddleware()
+      .concat(productApi.middleware)
+      .concat(AuthAPI.middleware), // Add the login API middleware,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
